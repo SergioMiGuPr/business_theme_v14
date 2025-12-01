d20df1889a555c69e54b792ded3a66969058a3de
Sergio <sergio.guedess@hotmail.com>
2025-12-01 15:17:56 +0100

    feat: New Logo

--- business_theme_v14/public/js/business_theme_v14.js
+++ business_theme_v14/public/js/business_theme_v14.js
@@ -1,14 +1,14 @@
 // business_theme_v14.js
 
-frappe.ready(function() {
+$(document).ready(function() {
     setTimeout(function() {
-        const navbarLogo = document.querySelector('.navbar-brand img');
+        const navbarLogo = document.querySelector('.app-logo');
         if (navbarLogo) {
             navbarLogo.src = '/assets/business_theme_v14/images/logo.png';
             navbarLogo.style.maxHeight = '40px';
         }
         
-        const sidebarLogo = document.querySelector('.app-logo img');
+        const sidebarLogo = document.querySelector('.sidebar-menu .app-logo');
         if (sidebarLogo) {
             sidebarLogo.src = '/assets/business_theme_v14/images/logo.png';
         }
@@ -16,7 +16,7 @@ frappe.ready(function() {
 });
 
 $(document).on('page-change', function() {
-    const navbarLogo = document.querySelector('.navbar-brand img');
+    const navbarLogo = document.querySelector('.app-logo');
     if (navbarLogo) {
         navbarLogo.src = '/assets/business_theme_v14/images/logo.png';
     }
